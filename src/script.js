e4f511d96303796d98a04573058b7afb35956f9c
Stefan <107488671+steff-mat@users.noreply.github.com>
2022-10-15 20:38:51 +0200

    restored basic movement

--- src/script.js
+++ src/script.js
@@ -2,15 +2,14 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let speed = 2;
-let x = 50;
-let y = 50;
+let x = 350;
+let y = 150;
 let velocityRightX = 0;
 let velocityLeftX = 0;
 let velocityUpY = 0;
 let velocityDownY = 0;
 
 function update() {
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     x += velocityRightX;
     x += velocityLeftX;
